0e8ebdae210d5414721cf5a4f9e1a5a46ab3bbc7
Chawanlak Jitman <chawanluck8659@gmail.com>
2024-11-18 15:38:10 +0700

    Dart

---
+++ reminder.dart
@@ -0,0 +1,14 @@
+void main() {
+  int num = 10; // ตัวตั้ง
+  int num1 = 3;   // ตัวหาร
+
+  // การคำนวณ
+  int quotient = num ~/ num1; // หาผลหาร (จำนวนเต็ม)
+  int remainder = num % num1; // หาเศษเหลือ
+
+  // แสดงผลลัพธ์
+  print('quotient = $quotient');
+  print('remainder = $remainder');
+}
+
+
